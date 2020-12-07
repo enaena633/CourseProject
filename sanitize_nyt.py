7c5abeaeb003462a07d76ee53f581a1f3f438e57
Matt McCarty <mdm12@illinois.edu>
2020-12-07 18:50:14 -0500

    Create consolidated_nyt.tsv which has one batch of NYT article paragraphs per day

--- sanitize_nyt.py
+++ sanitize_nyt.py
@@ -7,13 +7,13 @@ bush_regex = re.compile('Bush[^a-zA-Z]+')
 gore_regex = re.compile('Gore[^a-zA-Z]+')
 root = "nyt_corpus/data/2000"
 
-def parse_single_article(output_file, month, day, day_file):
+def parse_single_article(month, day, day_file):
     xmlFile = root + "/" + month + "/" + day + "/" + day_file
     fileContents = et.parse(xmlFile)
     body_xml = fileContents.find("./body/body.content/*[@class='full_text']")
 
     if body_xml is None:
-        return
+        return ""
 
     body_paragraphs = body_xml.findall("p")
     filtered_output = ""
@@ -24,19 +24,23 @@ def parse_single_article(output_file, month, day, day_file):
             paragraph_str = paragraph_str.replace("\n", "").strip()
             filtered_output = filtered_output + paragraph_str + " "
             
-    if filtered_output != "":
-        body_str = f"{2000}/{month}/{day}\t" + filtered_output + "\n"
-        output_file.write(body_str)
+    return filtered_output
 
 if __name__ == '__main__':
-    output_file = open("sanitized_nyt.tsv", "w")
+    output_file = open("consolidated_nyt.tsv", "w")
 
     for root, month_folders, month_files in os.walk(root):
         for month in month_folders:
             for subroot, day_folders, day_files in os.walk(root + "/" + month):
                 for day in day_folders:
                     for subsubroot, no_folders, day_files in os.walk(root + "/" + month + "/" + day):
+                        day_document = ""
+
                         for day_file in day_files:
-                            parse_single_article(output_file, month, day, day_file)
+                            single_document = parse_single_article(month, day, day_file)
+                            if single_document != "":
+                                day_document = day_document + single_document + " "
+                        
+                        output_file.write(day_document + "\n")
 
     output_file.close()
