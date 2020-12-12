c93e740f33a3a3477ba78d763d84c63e0c89cf7e
jackma46 <63521036+jackma46@users.noreply.github.com>
2020-12-12 16:29:08 -0500

    Change to calc-prior

--- calc_prior.py
+++ calc_prior.py
@@ -8,7 +8,7 @@ def calc_prior(significance):
     """ 
     Input an array of impact and significance. Positive significance means positive impact and vice versa. 
     """
-    prior = []
+    prior = None
     # separate the significance according to their impact
     positive_sigs = significance- .9
     negative_sigs = -significance - .9
@@ -24,12 +24,12 @@ def calc_prior(significance):
             if tot != 0:
                 pos_percent = pos_orientation / tot
                 if (pos_percent < 0.1):
-                   prior.append(negative_sigs[i]/np.sum(negative_sigs[i]))
+                    np.concatenate(prior, negative_sigs[i]/np.sum(negative_sigs[i]))
                 elif (pos_percent > 0.9):
-                    prior.append(positive_sigs[i]/np.sum(positive_sigs[i]))
+                    np.concatenate(prior, positive_sigs[i]/np.sum(positive_sigs[i]))
                 else:
-                    prior.append(positive_sigs[i]/np.sum(positive_sigs[i]))
-                    prior.append(negative_sigs[i]/np.sum(negative_sigs[i]))
+                    np.concatenate(prior, positive_sigs[i]/np.sum(positive_sigs[i]))
+                    np.concatenate(prior, negative_sigs[i]/np.sum(negative_sigs[i]))
                 
     return prior
 
